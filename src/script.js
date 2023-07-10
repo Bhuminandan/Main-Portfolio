2ceda9259df7165a49a4b9080c2c3bd79720a490
Bhumi <bhumione9545@gmail.com>
2023-07-10 18:23:38 +0530

    Adding bug fixes to main file

--- src/script.js
+++ src/script.js
@@ -12,7 +12,7 @@ const mobileNav = () => {
         isMobileNavOpen = !isMobileNavOpen;
         if(!isMobileNavOpen){
             mobileNav.style.display = 'flex';
-            document.body.style.overflowY = 'hiddden';
+            document.body.style.overflowY = 'hidden';
         } else {
             mobileNav.style.display = 'none';
             document.body.style.overflowY = 'auto';
